d10a63d9a38ade639c3f6d102baa9d44373b10ae
Sachini Dilanka <55729823+Sachie96D@users.noreply.github.com>
2026-06-24 14:26:51 +0530

    Update LoginTest.java

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,5 +1,19 @@
 package tests;
 
-public class LoginTest {
+import org.testng.annotations.Test;
+import base.BaseTest;
+import pages.LoginPage;
 
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void verifyLogin() {
+
+        LoginPage login = new LoginPage(driver);
+
+        login.login(
+                "student",
+                "Password123"
+        );
+    }
 }
